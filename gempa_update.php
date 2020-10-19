6de9446027d18f2f9b4fe965e9af7f456f675298
RoichatulJannah21 <47619170+RoichatulJannah21@users.noreply.github.com>
2020-10-19 23:39:42 +0700

    isset

--- gempa_update.php
+++ gempa_update.php
@@ -2,7 +2,6 @@
 include 'config.php';
 
 $idgempa = isset($_POST['idgempa']);
-<<<<<<< HEAD
 $date  = isset($_POST['date']);
 $time = isset($_POST['time']);
 $lat  = isset($_POST['lat']);
@@ -19,24 +18,6 @@ $tsun  =  isset($_POST['tsun']);
 $tsunami  = isset($_POST['tsunami']);
 $source_1  = isset($_POST['source_1']);
 $source_2  = isset($_POST['source_2']); 
-=======
-$date  = $_POST['date'];
-$time = $_POST['time'];
-$lat  = $_POST['lat'];
-$lon  = $_POST['lon'];
-$depth= $_POST['depth'];
-$mag  = $_POST['mag'];
-$lokasi_1  =$_POST['lokasi_1'];
-$lokasi_2  =$_POST['lokasi_2'];
-$felt_1  =  $_POST['felt_1'];
-$felt_2  =  $_POST['felt_2'];
-$akibat_1  =$_POST['akibat_1'];
-$akibat_2  =$_POST['akibat_2'];
-$tsun  =  $_POST['tsun'];
-$tsunami  =$_POST['tsunami'];
-$source_1  = $_POST['source_1'];
-$source_2  = $_POST['source_2']; 
->>>>>>> f60498ce42ca0c21f40cbc26a67b0105993a127d
 
 mysqli_query($db,"UPDATE gempa SET date='$date',
                                     time='$time',
